cad7334a2e472740f89657cf66ef3062398fe73a
JmmonJeremy <suc19002@byui.edu>
2024-10-09 12:48:19 -0700

    Final Code Set-up. Got the schema in the "/" path to generate a swagger doc without xml in it.

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,32 @@
 const routes = require('express').Router();
 const temple = require('./temple');
+const swagger = require('./swagger');
 
-routes.use('/', require('./swagger'));
+routes.get('/', (req, res) => {
+  /* #swagger.description = "Welcome to the LDS Temple API" */
+  /* #swagger.responses[200] = { 
+        description: "Success", 
+        '@schema': { 
+             "type": "object",
+            "properties": {
+              "documentationURL": {
+                "type": "string",
+                "example": "https://jmmonjeremy.github.io/Jeremy-s--API-Docs/"
+                } 
+              } 
+            } 
+          } 
+        }    
+  */ 
+  console.log("GET / route handler is being hit");
+  const docData = {
+    documentationURL: 'https://jmmonjeremy.github.io/Jeremy-s--API-Docs/',
+  };
+  res.send(docData);
+});
+
+routes.use('/', swagger);
 routes.use('/temples', temple);
-routes.use(
-  '/',
-  (docData = (req, res) => {
-    let docData = {
-      documentationURL: 'https://jmmonjeremy.github.io/Jeremy-s--API-Docs/',
-    };
-    res.send(docData);
-  })
-);
+
 
 module.exports = routes;
--- swagger.js
+++ swagger.js
@@ -1,7 +1,11 @@
 const swaggerAutogen = require('swagger-autogen')();
 
+// Check if NODE_ENV is correctly set
+console.log('Environment:', process.env.NODE_ENV);
+
 // Check the environment and set the host and scheme accordingly
 const isProduction = process.env.NODE_ENV === 'production';
+console.log('Is Production:', isProduction); // Check if it's true or false
 
 const doc = {
   info: {
@@ -12,18 +16,21 @@ const doc = {
     ? 'w4-temple-api-route-swagger-documentation.onrender.com'
     : 'localhost:8080',
   schemes: isProduction ? ['https'] : ['http'],
-};
+  };
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
-const routes = ['./routes/index.js'];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
 
 // USE #1 OR #2 NOT BOTH!!!
 // #1 Generate swagger.json
-swaggerAutogen(outputFile, endpointsFiles, doc);
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+  console.log('Swagger file generated successfully:', outputFile);
+}).catch((err) => {
+  console.error('Error generating swagger file:', err);
+});
 
 // #2 Run server file after it gets generated (like running npm start with it)
 // swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
